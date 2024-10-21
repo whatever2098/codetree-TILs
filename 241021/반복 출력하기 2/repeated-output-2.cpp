d36f997d88e0046a28a6a3421f22197729b9172c
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2024-10-21 09:52:38 +0900

    Title: 반복 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241021/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void print(int N){
+    if(N < 1){ return; }
+    
+    print(N - 1);
+    cout << "HelloWorld\n";
+}
+
+int main() {
+    int N;
+    cin >> N;
+    print(N);
+    return 0;
+}
