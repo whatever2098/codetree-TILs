d97b568cfd516627d63db865f74337aaed89b508
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-02-26 19:59:23 +0900

    Title: 비오는 날, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

--- 250226/비오는 날/rainy-day.py
+++ 250226/비오는 날/rainy-day.py
@@ -12,12 +12,10 @@ class Weather_Data:
 weather_datas = []
 for i in range (n):
     date, day, weather = tuple(input().split())
-    rainy_days = 0
     if weather == "Rain":
-        rainy_days += 1
         dates.append(date)
         weather_datas.append(Weather_Data(date, day, weather))
 
-for i in range (rainy_days):
+for i in range (len(weather_datas)):
     if weather_datas[i].date_ == sorted(dates)[0]:
         print(weather_datas[i].date_, weather_datas[i].day_, weather_datas[i].weather_)
