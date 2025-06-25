84ed4d6851dce86212aca6f4172a9b97133d841e
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-06-25 10:06:53 +0900

    Title: 007, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250625/007/007.py
+++ 250625/007/007.py
@@ -1,14 +1,30 @@
-secret_code, meeting_point, time = input().split()
-time = int(time)
-
 class zzseven:
-    def __init__ (self, secret_code, meeting_point, time):
+    def __init__(self, secret_code, meeting_point, time):
         self.sc = secret_code
         self.mp = meeting_point
         self.time = time
 
-member1 = zzseven("codetree", "L", 13)
+# 첫 줄 입력을 한 번만 읽어서 초기화
+secret_code, meeting_point, time_str = input().split()
+time_val = int(time_str)
+memberi = zzseven(secret_code, meeting_point, time_val)
 
-print("secret code : " + member1.sc)
-print("meeting point : " + member1.mp)
-print("time : " + str(member1.time))
+while True:
+    line = input().strip()
+    # 빈 줄이 들어오면 반복 종료
+    if not line:
+        break
+
+    # 새로 들어온 세 값을 파싱
+    secret_code, meeting_point, time_str = line.split()
+    time_val = int(time_str)
+
+    # 멤버 속성 업데이트
+    memberi.sc = secret_code
+    memberi.mp = meeting_point
+    memberi.time = time_val
+
+    # 업데이트된 상태 출력
+    print("secret code :", memberi.sc)
+    print("meeting point :", memberi.mp)
+    print("time :", memberi.time)
