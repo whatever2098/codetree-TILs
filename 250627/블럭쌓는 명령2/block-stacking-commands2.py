02abc7ec5f8e0f3a5bd443fd9100cad0a1436ba4
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-06-27 13:33:30 +0900

    Title: 블럭쌓는 명령2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250627/블럭쌓는 명령2/block-stacking-commands2.py
@@ -0,0 +1,30 @@
+'''n, k = map(int, input().split())
+commands = [tuple(map(int, input().split())) for _ in range(k)]
+
+n, k = map(int, input().split())
+
+blocks = [0 for _ in range(n + 1)]
+
+for a, b in range(k):
+  if a >= b or a <= b:
+    blocks[a:b + 1:-1] += 1
+    
+blocks.sort(reverse = True)
+print(blocks[1])'''
+
+n, k = tuple(map(int, input().split()))
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(k)
+]
+
+blocks = [0] * (n + 1)
+
+# 블럭을 특정 구간에 쌓아줍니다.
+for a, b in segments:
+    for i in range(a, b + 1):
+        blocks[i] += 1
+
+# 최댓값을 구합니다.
+max_num = max(blocks)
+print(max_num)
