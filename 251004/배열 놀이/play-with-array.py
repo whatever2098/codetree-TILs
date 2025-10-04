0b1a73432664530abe869e7646dd3f9561000130
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-10-04 23:25:16 +0900

    Title: 배열 놀이, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 251004/배열 놀이/play-with-array.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N, Q = input().split()
+element = input().split() #list
+
+instruction = ""
+for _ in range(Q):
+    if instruction == "1 a":
+        print(element[a])
+    elif instruction == "2 b":
+        if b in element:
+            print(element.index(b))
+        else: print(0)
+    elif instruction == "3 s e":
+        for i in range(s-1, e):
+            print(element[i] + " ")
+    
+
