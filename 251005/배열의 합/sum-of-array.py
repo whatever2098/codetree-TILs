bd520b6aa9d156fb509f33fd387b4a794b6b9de9
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-10-05 20:25:25 +0900

    Title: 배열의 합, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251005/배열의 합/sum-of-array.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+total = 0
+
+for i in range(4):
+    arr_temp = list(map(int, input().split()))
+    for j in range(4):
+        total += arr_temp[j]
+    print(total)
+    total = 0
