2c3b5c72970e687cf47c26d4593b09672535fd9f
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-10-05 19:18:09 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251005/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = list(map(int, input().split()))
+
+cnt = 0
+
+for i in range(N):
+    if arr[i] == 2:
+        cnt += 1
+        if cnt == 3:
+            print(i + 1)
+            break
