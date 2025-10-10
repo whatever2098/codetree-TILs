30a71f11b833f174a2bd56089d3d077dfc3b8de3
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-10-10 10:15:34 +0900

    Title: 두 개의 격자 비교하기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 251010/두 개의 격자 비교하기/compare-two-grid.py
+++ 251010/두 개의 격자 비교하기/compare-two-grid.py
@@ -7,10 +7,16 @@ second_grid = [[0] * (M + 1) for _ in range(N + 1)]
 
 new_grid = [[0] * (M + 1) for _ in range(N + 1)]
 
-for i in range(1, N + 1):
-    row = list(map(int, input().split()))
-    for j in range(1, M + 1):
-        first_grid[i][j] = row[j - 1]
+def read_grid(N, M):
+    grid = [[0] * (M + 1) for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        row = list(map(int, input().split()))
+        for j in range(1, M + 1):
+            grid[i][j] = row[j - 1]
+    return grid
+
+first_grid = read_grid(N, M)
+second_grid = read_grid(N, M)
 
 for i in range(1, N + 1):
     for j in range(1, M + 1):
