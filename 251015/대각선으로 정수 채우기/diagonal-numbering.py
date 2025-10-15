9a0d645f5d93a11df1019fb30722509c23352936
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-10-15 23:59:48 +0900

    Title: 대각선으로 정수 채우기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251015/대각선으로 정수 채우기/diagonal-numbering.py
+++ 251015/대각선으로 정수 채우기/diagonal-numbering.py
@@ -3,21 +3,21 @@ n, m = map(int, input().split())
 board = [[0] * m for _ in range(n)]
 num = 1
 
-for start_row in range(n):
-    i, j = start_row, 0
-    while i >= 0 and j < m:
+for start_col in range(m):
+    i, j = 0, start_col
+    while i < n and j >= 0:
         board[i][j] = num
         num += 1
-        i -= 1
-        j += 1
+        i += 1
+        j -= 1
 
-for start_col in range(1, m):
-    i, j = n - 1, start_col
-    while i >= 0 and j < m:
-        board[i][j] num
+for start_row in range(1, n):
+    i, j = start_row, m - 1
+    while i < n and j >= 0:
+        board[i][j] = num
         num += 1
-        i -= 1
-        j += 1
+        i += 1
+        j -= 1
 
 for row in board:
     print(*row)
