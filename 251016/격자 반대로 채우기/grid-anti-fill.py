321fdffb08eb2c045a92a5b1faa73e85512df59f
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-10-16 12:16:09 +0900

    Title: 격자 반대로 채우기, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

--- 251016/격자 반대로 채우기/grid-anti-fill.py
+++ 251016/격자 반대로 채우기/grid-anti-fill.py
@@ -6,13 +6,14 @@ n = int(input())
 board = [[0] * n for _ in range(n)]
 num = 1
 
-for j in range(n):  # 열(column) 기준 반복
-    if j % 2 == 0:  # 짝수열이면 위 → 아래
-        for i in range(n):
+# 오른쪽 끝 열(n-1)부터 왼쪽(0)으로 진행
+for offset, j in enumerate(range(n - 1, -1, -1)):
+    if offset % 2 == 0:           # 오른쪽에서 1,3,5...번째 열: 아래 -> 위
+        for i in range(n - 1, -1, -1):
             board[i][j] = num
             num += 1
-    else:           # 홀수열이면 아래 → 위
-        for i in range(n - 1, -1, -1):
+    else:                         # 2,4,6...번째 열: 위 -> 아래
+        for i in range(n):
             board[i][j] = num
             num += 1
 
