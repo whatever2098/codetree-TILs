837b3b9f91e0d5bf82a982624d600ecff1881de5
HyunjinLee <113659521+whatever2098@users.noreply.github.com>
2025-11-19 14:12:47 +0900

    Title: 배열의 평균, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251119/배열의 평균/ave-of-array.py
+++ 251119/배열의 평균/ave-of-array.py
@@ -1,8 +1,10 @@
 import sys
 input = sys.stdin.readline
 
+# 2x4 배열 선언
 arr = [list(map(int, input().split())) for _ in range(2)]
 
+# 각 행 평균 출력 (2행)
 for i in range(2):
     s = 0
     for j in range(4):
@@ -10,6 +12,7 @@ for i in range(2):
     print(s / 4, end=" ")
 print()
 
+# 각 열 평균 출력 (4열)
 for j in range(4):
     s = 0
     for i in range(2):
@@ -17,8 +20,10 @@ for j in range(4):
     print(s / 2, end=" ")
 print()
 
+# 전체 평균
 total = 0
 for i in range(2):
     for j in range(4):
         total += arr[i][j]
+
 print(total / 8)
